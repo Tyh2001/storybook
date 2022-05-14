ed594781458d7dcdc7b16b0540808aa23e74ef61
15227241216 <1469442737@qq.com>
2022-05-14 12:45:03 +0800

    docs: 更新导航栏跳转

--- docs/.vitepress/utils/nav.ts
+++ docs/.vitepress/utils/nav.ts
@@ -12,22 +12,22 @@ export const nav = [
   {
     text: '文章记录',
     activeMatch: '/article/',
-    link: '/article/article/article-0'
+    link: '/article/milepost/milepost-2021'
   },
   {
     text: 'Github',
     activeMatch: 'https://github.com/Tyh2001/tyh-blog',
     link: 'https://github.com/Tyh2001/tyh-blog'
-  }
+  },
   // {
   //   text: '关于我',
-  //   ariaLabel: 'Language Menu',
-  //   children: [
-  //     { text: '个人网站', link: 'https://tianyuhao.cn' },
-  //     { text: 'Gitee', link: 'https://gitee.com/tyh666999' },
-  //     { text: '微博', link: 'https://weibo.com/tyh2001' },
-  //     { text: '哔哩哔哩', link: 'https://space.bilibili.com/246484504?spm_id_from=333.1007.0.0' },
-  //     { text: '掘金', link: 'https://juejin.cn/user/2243446742456888' },
+  //   // activeMatch: 'abc',
+  //   item: [
+  //     { text: '个人网站', activeMatch: 'https://tianyuhao.cn', link: 'https://tianyuhao.cn' },
+  //     // { text: 'Gitee', link: 'https://gitee.com/tyh666999' },
+  //     // { text: '微博', link: 'https://weibo.com/tyh2001' },
+  //     // { text: '哔哩哔哩', link: 'https://space.bilibili.com/246484504?spm_id_from=333.1007.0.0' },
+  //     // { text: '掘金', link: 'https://juejin.cn/user/2243446742456888' },
   //   ]
   // },
   // {
