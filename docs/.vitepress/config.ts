e6fe471751184943d0f0e4e28b1d47be4fade75b
tyh2001 <1469442737@qq.com>
2022-05-25 16:00:44 +0800

    feat: 更新配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vitepress'
 import { nav } from './utils/nav'
 import { sidebar } from './utils/sidebar'
-import docsearch from '@docsearch/js'
 
 const config = defineConfig({
   title: 'Tyh',
   head: [
-    ['link', { rel: 'icon', href: 'https://tianyuhao.cn/images/my.png' }]
+    ['link', { rel: 'icon', href: 'https://tianyuhao.cn/images/my.png' }],
+    ['link', { rel: 'stylesheet', src: 'https://cdn.jsdelivr.net/npm/@docsearch/css@3' }],
+    ['script', { src: 'https://cdn.jsdelivr.net/npm/@docsearch/js@3' }],
   ],
   description: 'Tyh,Tyh2001,tyh,博客,Admin,前端开发,javascript,php,node,jquery,git,python,java,sql,mysql,linux,go语言,golang,前端,教程,软件,编程,互联网,tyh-ui,TYH-UI,tyh,TYH,ui,UI,element,框架,组件库,组件,vue,vue3,码云,NPM,npm,components,js,github,gitee,npm,vant,element3,vite,viewUI,tyh-ui,tyh ui,ui tyh,tyh-ui2,tyh-ui,tyh2001的个人网站',
   base: '/blog/',
@@ -15,9 +16,9 @@ const config = defineConfig({
     nav,
     sidebar,
     algolia: {
-      appId: 'R2IYF7ETH7',
-      apiKey: '599cec31baffa4868cae4e79f180729b',
-      indexName: 'docsearch'
+      indexName: 'tianyuhao',
+      appId: 'OKXOHGNLXA',
+      apiKey: 'ab89abeb15d59a23fa8ece1be1f94a07',
     }
   }
 })
