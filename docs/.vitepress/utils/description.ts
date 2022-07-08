778e4ee182b3517f8bbabc834b35155e9ec3880e
15227241216 <1469442737@qq.com>
2022-07-08 12:20:28 +0800

    docs: 全局格式化

--- docs/.vitepress/utils/description.ts
+++ docs/.vitepress/utils/description.ts
@@ -1,1 +1,2 @@
-export const description = 'Tyh,Tyh2001,tyh,博客,Admin,前端开发,javascript,php,node,jquery,git,python,java,sql,mysql,linux,go语言,golang,前端,教程,软件,编程,互联网,tyh-ui,TYH-UI,tyh,TYH,ui,UI,element,框架,组件库,组件,vue,vue3,码云,NPM,npm,components,js,github,gitee,npm,vant,element3,vite,viewUI,tyh-ui,tyh ui,ui tyh,tyh-ui2,tyh-ui,tyh2001的个人网站'
+export const description =
+  'Tyh,Tyh2001,tyh,博客,Admin,前端开发,javascript,php,node,jquery,git,python,java,sql,mysql,linux,go语言,golang,前端,教程,软件,编程,互联网,tyh-ui,TYH-UI,tyh,TYH,ui,UI,element,框架,组件库,组件,vue,vue3,码云,NPM,npm,components,js,github,gitee,npm,vant,element3,vite,viewUI,tyh-ui,tyh ui,ui tyh,tyh-ui2,tyh-ui,tyh2001的个人网站'
