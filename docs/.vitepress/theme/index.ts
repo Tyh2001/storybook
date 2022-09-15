a145dc2f8c6942e186a39180d9c483ba46cd18dd
tyh2001 <1469442737@qq.com>
2022-09-15 11:00:19 +0800

    chore: 更新路由错误，优化主题样式 :mega::mega::mega:

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import FightingDesign from 'fighting-design'
 import 'fighting-design/dist/index.css'
+import './style/index.scss'
 
 export default {
   ...DefaultTheme,
