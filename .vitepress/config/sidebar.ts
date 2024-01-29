5cfb98af1621e86023632fcb4239ce8d369a43b7
Tyh2001 <1469442737@qq.com>
2024-01-29 15:09:35 +0800

    docs: 优化格式

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -10,70 +10,83 @@ export const sidebar = {
     {
       text: 'JavaScript',
       items: [
-        { text: '基础', link: '/docs/javascript/basic' },
-        { text: 'DOM 相关', link: '/docs/javascript/dom' },
-        { text: '函数', link: '/docs/javascript/function' },
-        { text: '数据类型', link: '/docs/javascript/data-type' },
-        { text: '字符串方法', link: '/docs/javascript/methods-string' },
-        { text: '数组方法', link: '/docs/javascript/methods-array' },
-        { text: '数组迭代', link: '/docs/javascript/array-iteration' },
-        { text: '对象方法', link: '/docs/javascript/methods-object' },
-        { text: '日期对象', link: '/docs/javascript/date-object' },
-        { text: '数学对象', link: '/docs/javascript/math-object' },
-        { text: '异步函数', link: '/docs/javascript/fun-async' },
-        { text: '面向对象编程', link: '/docs/javascript/fun-prototype' },
-        { text: 'Class 类的使用', link: '/docs/javascript/fun-class' },
-        { text: 'JavaScript API', link: '/docs/javascript/javascript-api' },
-        { text: '生成器', link: '/docs/javascript/generator' },
-        { text: 'proxy 代理', link: '/docs/javascript/proxy' },
-        { text: '事件循环', link: '/docs/javascript/event-loop' }
+        { text: '基础', link: '/docs/javascript/1' },
+        { text: 'DOM', link: '/docs/javascript/2' },
+        { text: '函数', link: '/docs/javascript/3' },
+        { text: '数据类型', link: '/docs/javascript/4' },
+        { text: '字符串方法', link: '/docs/javascript/5' },
+        { text: '数组方法', link: '/docs/javascript/6' },
+        { text: '数组迭代', link: '/docs/javascript/7' },
+        { text: '对象方法', link: '/docs/javascript/8' },
+        { text: '日期对象', link: '/docs/javascript/9' },
+        { text: '数学对象', link: '/docs/javascript/10' },
+        { text: '异步函数', link: '/docs/javascript/11' },
+        { text: '面向对象编程', link: '/docs/javascript/12' },
+        { text: 'Class 类的使用', link: '/docs/javascript/13' },
+        { text: 'JavaScript API', link: '/docs/javascript/14' },
+        { text: '生成器', link: '/docs/javascript/15' },
+        { text: 'Proxy', link: '/docs/javascript/16' },
+        { text: '事件循环', link: '/docs/javascript/17' }
       ]
     },
     {
       text: 'Vue',
       items: [
-        { text: 'Vue2 基础', link: '/docs/vue/vue2' },
-        { text: 'Vue3 基础', link: '/docs/vue/vue3' },
-        { text: 'Vue3 Router', link: '/docs/vue/vue3-router' },
-        { text: 'Vuex', link: '/docs/vue/vuex' },
-        { text: 'Pinia', link: '/docs/vue/pinia' }
+        { text: 'Vue2 基础', link: '/docs/vue/1' },
+        { text: 'Vue3 基础', link: '/docs/vue/2' },
+        { text: 'Vue3 Router', link: '/docs/vue/3' },
+        { text: 'Vuex', link: '/docs/vue/4' },
+        { text: 'Pinia', link: '/docs/vue/5' }
       ]
     },
     {
       text: 'React',
       items: [
-        { text: '基础', link: '/docs/react/react-1' },
-        { text: 'Router', link: '/docs/react/react-2' },
-        { text: '样式处理', link: '/docs/react/react-3' },
-        { text: '项目搭建', link: '/docs/react/react-4' },
-        { text: 'API', link: '/docs/react/react-5' },
-        { text: 'Zustand', link: '/docs/react/react-6' },
+        { text: '基础', link: '/docs/react/1' },
+        { text: 'Router', link: '/docs/react/2' },
+        { text: '样式处理', link: '/docs/react/3' },
+        { text: '项目搭建', link: '/docs/react/4' },
+        { text: 'API', link: '/docs/react/5' },
+        { text: 'Zustand', link: '/docs/react/6' },
       ]
     },
     {
       text: 'TypeScript',
       items: [
-        { text: '类型', link: '/docs/typescript/type' },
-        { text: '其它', link: '/docs/typescript/other' }
+        { text: '类型', link: '/docs/typescript/1' },
+        { text: '其它', link: '/docs/typescript/2' }
       ]
     },
     {
       text: 'Nest',
-      items: [{ text: '基础', link: '/docs/nest/nest-1' }]
+      items: [{ text: '基础', link: '/docs/nest/1' }]
+    },
+    {
+      text: 'Node',
+      items: [{ text: 'Node', link: '/docs/node/1' }]
     },
     {
       text: '服务端',
       items: [
-        { text: '服务器配置', link: '/docs/server/server-1' },
-        { text: 'Mysql', link: '/docs/server/server-2' },
+        { text: '服务器配置', link: '/docs/server/1' },
+        { text: 'Mysql', link: '/docs/server/2' },
+        { text: 'Redis', link: '/docs/server/2' },
+        { text: 'Nginx', link: '/docs/server/2' },
       ]
     },
     {
       text: '操作系统',
       items: [
-        { text: 'Windows', link: '/docs/system/system-1' },
-        { text: 'PowerShell', link: '/docs/system/system-2' },
-        { text: 'MacOS', link: '/docs/system/system-3' },
+        { text: 'Windows', link: '/docs/system/1' },
+        { text: 'PowerShell', link: '/docs/system/2' },
+        { text: 'MacOS', link: '/docs/system/3' },
+      ]
+    },
+    {
+      text: 'Swift',
+      items: [
+        { text: '安装编辑器', link: '/docs/swift/1' },
+        { text: '打印 Hello Swift', link: '/docs/swift/2' },
       ]
     }
   ],
@@ -81,9 +94,9 @@ export const sidebar = {
     {
       text: '里程碑',
       items: [
-        { text: '里程碑 2021', link: '/article/milepost/milepost-2021' },
-        { text: '里程碑 2022', link: '/article/milepost/milepost-2022' },
-        { text: '里程碑 2024', link: '/article/milepost/milepost-2024' },
+        { text: '里程碑 2021', link: '/article/milepost/2021' },
+        { text: '里程碑 2022', link: '/article/milepost/2022' },
+        { text: '里程碑 2024', link: '/article/milepost/2024' },
       ]
     },
     {
@@ -96,111 +109,111 @@ export const sidebar = {
     {
       text: '我的文章',
       items: [
-        { text: '记事本', link: '/article/article/article-0' },
-        { text: '别再写循环套循环了', link: '/article/article/article-1' },
-        { text: '关于组件库', link: '/article/article/article-2' },
-        { text: '看书的分享', link: '/article/article/article-3' },
-        { text: 'GitHubPages 部署项目', link: '/article/article/article-4' },
-        { text: 'highlightjs 使用方法', link: '/article/article/article-5' },
-        { text: 'Node.js 版本切换', link: '/article/article/article-6' },
+        { text: '记事本', link: '/article/article/1' },
+        { text: '关于组件库', link: '/article/article/2' },
+        { text: '看书的分享', link: '/article/article/3' },
+        { text: 'GitHubPages 部署项目', link: '/article/article/4' },
+        { text: 'highlightjs 使用方法', link: '/article/article/5' },
+        { text: 'Node.js 版本切换', link: '/article/article/6' },
         {
           text: 'Vue3 + pnpm 搭建 monorepo 项目',
-          link: '/article/article/article-7'
+          link: '/article/article/7'
         },
-        { text: 'Git 基础知识', link: '/article/article/article-8' },
-        { text: 'Vue3 的源码怎么看', link: '/article/article/article-9' },
+        { text: 'Git 基础知识', link: '/article/article/8' },
+        { text: 'Vue3 的源码怎么看', link: '/article/article/9' },
         {
           text: 'Vite + TS 配置 .env 环境文件',
-          link: '/article/article/article-10'
+          link: '/article/article/10'
         },
         {
           text: 'Vue3 开源组件库 Fighting Design 成员招募',
-          link: '/article/article/article-11'
+          link: '/article/article/11'
         },
         {
           text: '想参与开源贡献吗？Fighting Design 欢迎加入！',
-          link: '/article/article/article-12'
+          link: '/article/article/12'
         },
         {
           text: 'Github 如何提交 PR？',
-          link: '/article/article/article-13'
+          link: '/article/article/13'
         },
         {
           text: '我是如何学习的？',
-          link: '/article/article/article-14'
+          link: '/article/article/14'
         },
         {
           text: '分享一些我的开源项目',
-          link: '/article/article/article-15'
+          link: '/article/article/15'
         },
         {
           text: '关于图片加载，你需要学习一下',
-          link: '/article/article/article-16'
+          link: '/article/article/16'
         },
         {
           text: '远程计算机或设备将不接受链接-解决方案',
-          link: '/article/article/article-17'
+          link: '/article/article/17'
         },
         {
           text: 'Linux 初级自动化打包部署',
-          link: '/article/article/article-18'
+          link: '/article/article/18'
         },
         {
           text: 'Vue3 组件库 Fighting Design 欢迎加入',
-          link: '/article/article/article-19'
+          link: '/article/article/19'
         },
         {
           text: '纯原生开发 Web Components 超详细分享',
-          link: '/article/article/article-20'
-        }
+          link: '/article/article/20'
+        },
+        { text: '别再写循环套循环了', link: '/article/article/21' },
       ]
     },
     {
       text: 'JavaScript 设计模式',
       items: [
-        { text: '基础知识', link: '/article/design-mode/design-1' },
-        { text: '高阶函数', link: '/article/design-mode/design-2' },
-        { text: '单例模式', link: '/article/design-mode/design-3' },
-        { text: '策略模式', link: '/article/design-mode/design-4' },
-        { text: '代理模式', link: '/article/design-mode/design-5' },
-        { text: '迭代器模式', link: '/article/design-mode/design-6' },
-        { text: '发布订阅模式', link: '/article/design-mode/design-7' },
-        { text: '命令模式', link: '/article/design-mode/design-8' },
-        { text: '组合模式', link: '/article/design-mode/design-9' },
-        { text: '模板方法模式', link: '/article/design-mode/design-10' },
-        { text: '享元模式', link: '/article/design-mode/design-11' },
-        { text: '职责链模式', link: '/article/design-mode/design-12' },
-        { text: '状态模式', link: '/article/design-mode/design-13' },
-        { text: '适配器模式', link: '/article/design-mode/design-14' }
+        { text: '基础知识', link: '/article/design-mode/1' },
+        { text: '高阶函数', link: '/article/design-mode/2' },
+        { text: '单例模式', link: '/article/design-mode/3' },
+        { text: '策略模式', link: '/article/design-mode/4' },
+        { text: '代理模式', link: '/article/design-mode/5' },
+        { text: '迭代器模式', link: '/article/design-mode/6' },
+        { text: '发布订阅模式', link: '/article/design-mode/7' },
+        { text: '命令模式', link: '/article/design-mode/8' },
+        { text: '组合模式', link: '/article/design-mode/9' },
+        { text: '模板方法模式', link: '/article/design-mode/10' },
+        { text: '享元模式', link: '/article/design-mode/11' },
+        { text: '职责链模式', link: '/article/design-mode/12' },
+        { text: '状态模式', link: '/article/design-mode/13' },
+        { text: '适配器模式', link: '/article/design-mode/14' }
       ]
     },
     {
       text: 'Vue.js 设计与实现',
       items: [
-        { text: '第一章 权衡的艺术', link: '/article/vue-design/page-1' },
+        { text: '第一章 权衡的艺术', link: '/article/vue-design/1' },
         {
           text: '第二章 框架设计的核心要素',
-          link: '/article/vue-design/page-2'
+          link: '/article/vue-design/2'
         },
         {
           text: '第三章 Vue.js 3 的设计思路',
-          link: '/article/vue-design/page-3'
+          link: '/article/vue-design/3'
         },
         {
           text: '第四章 响应系统的作用与实现',
-          link: '/article/vue-design/page-4'
+          link: '/article/vue-design/4'
         }
       ]
     },
     {
       text: '前端面试题',
       items: [
-        { text: '更新面试题', link: '/article/ques/ques-new' },
-        { text: 'Web 相关', link: '/article/ques/ques-web' },
-        { text: 'Html 相关', link: '/article/ques/ques-html' },
-        { text: 'Css 相关', link: '/article/ques/ques-css' },
-        { text: 'JavaScript 相关', link: '/article/ques/ques-javascript' },
-        { text: 'Vue2 相关', link: '/article/ques/ques-vue2' }
+        { text: '更新面试题', link: '/article/ques/1' },
+        { text: 'Web 相关', link: '/article/ques/2' },
+        { text: 'Html 相关', link: '/article/ques/3' },
+        { text: 'Css 相关', link: '/article/ques/4' },
+        { text: 'JavaScript 相关', link: '/article/ques/5' },
+        { text: 'Vue2 相关', link: '/article/ques/6' }
       ]
     }
   ]
