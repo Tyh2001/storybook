6cd8575e379c4c562a3057b8c576e9b2795e581b
Tyh2001 <Tyh2001@users.noreply.github.com>
2024-01-30 11:07:36 +0000

    deploy: 5c4f206144019a9462164506b1a7a29960c4f688

---
+++ assets/docs_swift_2.md.2c0342a6.js
@@ -0,0 +1,14 @@
+import{_ as s,o as n,c as a,a as l}from"./app.6a6feea0.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[{"level":2,"title":"打印 Hello Swift","slug":"打印-hello-swift","link":"#打印-hello-swift","children":[]}],"relativePath":"docs/swift/2.md"}'),e={name:"docs/swift/2.md"},p=l(`<h2 id="打印-hello-swift" tabindex="-1">打印 Hello Swift <a class="header-anchor" href="#打印-hello-swift" aria-hidden="true">#</a></h2><p>使用 Text 可以在页面上显示文字内容</p><div class="language-swift"><button title="Copy Code" class="copy"></button><span class="lang">swift</span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#89DDFF;font-style:italic;">import</span><span style="color:#BABED8;"> </span><span style="color:#FFCB6B;">SwiftUI</span></span>
+<span class="line"><span style="color:#89DDFF;font-style:italic;">import</span><span style="color:#BABED8;"> </span><span style="color:#FFCB6B;">SwiftData</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#C792EA;">struct</span><span style="color:#BABED8;"> </span><span style="color:#FFCB6B;">ContentView</span><span style="color:#89DDFF;">:</span><span style="color:#BABED8;"> </span><span style="color:#FFCB6B;">View </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#BABED8;">    </span><span style="color:#F78C6C;">var</span><span style="color:#BABED8;"> body: </span><span style="color:#F78C6C;">some</span><span style="color:#BABED8;"> View </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#BABED8;">      </span><span style="color:#82AAFF;">Text</span><span style="color:#89DDFF;">(</span><span style="color:#89DDFF;">&quot;</span><span style="color:#C3E88D;">Hello Swift</span><span style="color:#89DDFF;">&quot;</span><span style="color:#89DDFF;">)</span></span>
+<span class="line"><span style="color:#BABED8;">    </span><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">#Preview</span><span style="color:#BABED8;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#BABED8;">    </span><span style="color:#82AAFF;">ContentView</span><span style="color:#89DDFF;">()</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span></code></pre></div>`,3),o=[p];function t(c,r,i,F,D,y){return n(),a("div",null,o)}const d=s(e,[["render",t]]);export{_ as __pageData,d as default};
