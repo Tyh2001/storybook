ba141dbf8dc354b599939a5d2e0eef33b5787326
Tyh2001 <1469442737@qq.com>
2024-04-22 18:02:03 +0800

    docs: 更新游戏

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -287,6 +287,7 @@ export const sidebar = {
       text: '记录',
       items: [
         { text: '摘抄', link: '/record/extract/1' },
+        { text: '游戏收藏', link: '/record/extract/2' },
       ]
     }
   ]
